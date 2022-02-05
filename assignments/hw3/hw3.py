6369a69380a60d654ff3d98a937893cd54be475d
alex toscano <toscanoae@g.cofc.edu>
2022-02-04 21:30:48 -0500

    completed hw3.

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,35 +1,59 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Alex Toscano
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: using loops to do arithmetic
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    grades = eval(input("how many grades will you enter? "))
+    acc = 0
+    for i in range(grades):
+        total = eval(input("enter grade: "))
+        acc = acc + total
+    print("average is", acc / grades)
 
 
 def tip_jar():
-    pass
+    acc = 0
+    for i in range(5):
+        tip = eval(input("how much would you like to donate? "))
+        acc = acc + tip
+    print("total tips:", acc)
 
 
 def newton():
-    pass
+    x = eval(input("What number do you want to square root? "))
+    improvements = eval(input("How many times should we improve the approximation? "))
+    approx = x
+    for i in range(improvements):
+        approx = (approx + (x / approx)) / 2
+    print(approx)
 
 
 def sequence():
-    pass
+    terms = eval(input("how many terms would you like? "))
+    for i in range(terms):
+        print(1 + (i // 2 * 2), end=" ")
 
 
 def pi():
-    pass
+    n = eval(input("how many terms in series? "))
+    acc = 1
+    for i in range(n):
+        num = 2 + ((i // 2) * 2)
+        den = 1 + (((i + 1) // 2) * 2)
+        acc = acc * (num / den)
+    print(acc * 2)
 
 
 if __name__ == '__main__':
-    pass
+    # average()
+    # tip_jar()
+    # newton()
+    # sequence()
+    pi()
