59ebf5abd82782b0c0954507dd14a3c6c663062a
alex toscano <toscanoae@g.cofc.edu>
2022-03-18 20:19:32 -0400

    hw8

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,42 +1,67 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Alex Toscano
+hw8.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
+from graphics import *
+import math
 
 
 def add_ten(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = nums[i] + 10
 
 
 def square_each(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
 
 
 def sum_list(nums):
-    pass
+    acc = 0
+    for i in range(len(nums)):
+        acc = acc + nums[i]
+    return acc
 
 
 def to_numbers(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = float(nums[i])
 
 
 def sum_of_square(nums):
-    pass
+    filein = input("Enter name of infile: ")
+    fileout = input("Enter name of outfile: ")
+    infile = open(filein, "r")
+    outfile = open(fileout, "w+")
+    for line in infile:
+        line = line.split()
+        to_numbers(line)
+        square_each(line)
+        outfile.write("Sum of squares = " + str(sum_list(line)) + "\n")
 
 
 def starter(weight, wins):
-    pass
+    weight = eval(input("Enter players weight: "))
+    wins = eval((input("Enter players total wins: ")))
+    if 150 <= weight < 160 and wins >= 5:
+        print("Can start!")
+    elif weight > 199 or wins > 20:
+        print("Can start!")
+    else:
+        print("Sit on bench please!")
 
 
 def leap_year(year):
-    pass
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else:
+        return False
 
 
 def circle_overlap():
@@ -46,7 +71,7 @@ def circle_overlap():
     width = 10
     height = 10
     win.setCoords(0, 0, width, height)
-
+    # circle 1
     center = win.getMouse()
     circumference_point = win.getMouse()
     radius = math.sqrt(
@@ -54,13 +79,33 @@ def circle_overlap():
     circle_one = Circle(center, radius)
     circle_one.setFill("light blue")
     circle_one.draw(win)
+    # circle 2
+    center2 = win.getMouse()
+    circumference_point2 = win.getMouse()
+    radius2 = math.sqrt(
+        (center2.getX() - circumference_point2.getX()) ** 2 + (center2.getY() - circumference_point2.getY()) ** 2)
+    circle_two = Circle(center2, radius2)
+    circle_two.setFill("light green")
+    circle_two.draw(win)
 
     win.getMouse()
 
 
 def did_overlap(circle_one, circle_two):
-    pass
+    d = math.sqrt((circle_one.getX() - circle_two.getX()) ** 2 + (circle_one.getY() - circle_two.getY()) ** 2)
+    if d <= circle_one.getRadius() + circle_two.getRadius():
+        over = Text(Point(2, 6), "The circle overlaps!")
+    else:
+        under = Text(Point(2, 6), "The circle does not overlap!")
 
 
 if __name__ == '__main__':
-    pass
+    # add_ten()
+    # square_each()
+    # sum_list()
+    # to_numbers()
+    # sum_of_square()
+    # starter()
+    # leap_year()
+    circle_overlap()
+    did_overlap()
